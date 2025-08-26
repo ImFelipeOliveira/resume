054bb16313a8d950a17da0880f650e55ddbe9f75
Felipe Oliveira <flipeaz342@gmail.com>
2025-08-26 00:24:07 -0400

    fix

--- src/message-broker/queue/reply-queue.ts
+++ src/message-broker/queue/reply-queue.ts
@@ -1,18 +1,14 @@
 import {env} from "../../config/env";
 import {RabbitMQ} from "../../adapters/rabbitmq";
 import {IAdapterFactory} from "../../adapters/interfaces/IAdapter-factory";
-import {IServiceFactory} from "../../services/interfaces/IService-factory";
-import {BaileysService} from "../../services/baileys-service";
 import {replyPayload} from "../workers/summary-worker";
 
 export class ReplyQueue {
     private readonly queueName: string = env.rabbitmq.queues.reply!
     private readonly rabbitMQ: Promise<RabbitMQ>;
-    private baileyService: BaileysService
 
-    constructor(private adapterFactory: IAdapterFactory, private serviceFactory: IServiceFactory) {
+    constructor(private adapterFactory: IAdapterFactory) {
         this.rabbitMQ = this.adapterFactory.createRabbitMQAdapter()
-        this.baileyService = this.serviceFactory.createBaileysService()
     }
 
     async enqueue(payload: replyPayload) {
--- src/message-broker/workers/reply-worker.ts
+++ src/message-broker/workers/reply-worker.ts
@@ -15,7 +15,7 @@ export class ReplyWorker {
     async processTask() {
         const adapter = await this.rabbitMQ
         await adapter.consume(this.queueName, async (msg: any) => {
-            const input: replyPayload = JSON.parse(msg.content.toString())
+            const input: replyPayload = JSON.parse(msg)
             await this.baileysService.sendMessage(input.groupId, input.message)
         })
     }
